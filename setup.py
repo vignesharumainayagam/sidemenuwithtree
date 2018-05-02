1bde201a5039e9add6ae7370024eac15f4eeabbd
Noor Mohamed <aknoormohamed@gmail.com>
2018-05-02 13:57:01 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in school_theme/__init__.py
+# get version from version variable in erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('school_theme/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('school_theme/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='school_theme',
 	version=version,
-	description='school_theme description',
-	author='Noor',
-	author_email='noor@valiantsystems.com',
+	description='Open Source ERP',
+	author='Frappe Technologies',
+	author_email='info@tridots.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
